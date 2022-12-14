afee3efc84cf9086092f98cce312290692f94d2a
Emre Yılmaz <117239489+emreyilmaz1912@users.noreply.github.com>
2022-12-14 12:50:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Kombinasyon Hesaplama Programına Hoşgeldiniz :");
+        int n, r, combination;
+        int nProduct = 1, rProduct = 1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n sayısını giriniz :");
+        n = input.nextInt();
+
+        System.out.print("r sayısını giriniz :");
+        r = input.nextInt();
+
+        for (int i = 1; i <= r; i++){
+            rProduct *= i;
+            nProduct *= n;
+            n--;
+        }
+
+        combination = nProduct / rProduct;
+        System.out.print("C(n,r)) " + combination);
+
     }
 }
